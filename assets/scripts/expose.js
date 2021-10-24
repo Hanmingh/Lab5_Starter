10e5735a101d316af6d46eefa8793a16ac1fcbf8
Xuan-Wang-Summer <xuw009@ucsd.edu>
2021-10-23 17:21:03 -0700

    test

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,23 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
-}
+  const volumnChoose = document.getElementById('volume');
+  var allImg = document.querySelector('img');
+
+  volumnChoose.addEventListener('change', (event) => {
+    const v = Number(event.Target.value);
+    var vImg = allImg[1];
+    if(v === 0){
+      vImg.src = 'assets/icons/volume-level-0.svg';
+    }
+    else if(v < 33){
+      vImg.src = 'assets/icons/volume-level-1.svg';
+    }
+    else if(v < 67){
+      vImg.src = 'assets/icons/volume-level-2.svg';
+    }
+    else{
+      vImg.src = 'assets/icons/volume-level-3.svg';
+    }
+  })
+  }
