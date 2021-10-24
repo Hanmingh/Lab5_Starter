c40ef7b09f907ee0e58537a35a649f375c119dd3
Hanming Hu <hhu@ucsd.edu>
2021-10-23 21:27:00 -0700

    Update explore.js

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -4,32 +4,59 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
     var synth = window.speechSynthesis;
-
+    const face = document.querySelector('img');
     var voiceSelect = document.querySelector('select');
-    
     var voices = [];
-    
+
     function populateVoiceList() {
       voices = synth.getVoices();
-    
       for(var i = 0; i < voices.length ; i++) {
         var option = document.createElement('option');
         option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
-    
         if(voices[i].default) {
           option.textContent += ' -- DEFAULT';
         }
-    
         option.setAttribute('data-lang', voices[i].lang);
         option.setAttribute('data-name', voices[i].name);
         voiceSelect.appendChild(option);
       }
     }
-    
+
+    function Mouth(){
+      var amISpeaking = synth.speaking;
+      if(amISpeaking == true){
+        face.src = 'assets/images/smiling-open.png';
+      }
+      else{
+        face.src = 'assets/images/smiling.png';
+      }
+    }
+
+    setInterval(Mouth, 100);
+
     populateVoiceList();
     if (speechSynthesis.onvoiceschanged !== undefined) {
       speechSynthesis.onvoiceschanged = populateVoiceList;
     }
+
+    const input = document.querySelector('textarea');
+    const log = document.getElementById('text-to-speak');
+    input.addEventListener('input', updateValue);
+    function updateValue(e) {
+      log.textContent = e.target.value;
+    }
+  
+    const button = document.querySelector('button');
+    button.addEventListener('click', event => {
+      var utterThis = new SpeechSynthesisUtterance(log.textContent);
+      var selectedOption = voiceSelect.selectedOptions[0].getAttribute('data-name');
+      for(var i = 0; i < voices.length ; i++) {
+        if(voices[i].name === selectedOption) {
+          utterThis.voice = voices[i];
+        }
+      }
+      synth.speak(utterThis);
+    });
 }
 
 
